5c2ce07ae935fa1babf5ad2bad3d0ca88d2e4c07
Vitor Pamplona <vitor@vitorpamplona.com>
2022-10-03 10:24:49 -0400

    Adding a LibraryLoader for other files in the same directory of the current cql file.

--- src/main/kotlin/org/pathcheck/intellij/cql/CqlExternalAnnotator.kt
+++ src/main/kotlin/org/pathcheck/intellij/cql/CqlExternalAnnotator.kt
@@ -3,19 +3,15 @@ package org.pathcheck.intellij.cql
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.ExternalAnnotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
-import org.cqframework.cql.cql2elm.CqlCompiler
-import org.cqframework.cql.cql2elm.CqlCompilerException
-import org.cqframework.cql.cql2elm.LibraryManager
-import org.cqframework.cql.cql2elm.ModelManager
+import org.cqframework.cql.cql2elm.*
+import org.cqframework.cql.cql2elm.quick.FhirLibrarySourceProvider
 import org.cqframework.cql.elm.tracking.TrackBack
 import java.io.IOException
 
 class CqlExternalAnnotator : ExternalAnnotator<PsiFile?, List<CqlCompilerException>?>() {
-    private var modelManager: ModelManager? = null
-    private var libraryManager: LibraryManager? = null
-
     /** Called first; return file  */
     override fun collectInformation(file: PsiFile): PsiFile {
         return file
@@ -25,33 +21,35 @@ class CqlExternalAnnotator : ExternalAnnotator<PsiFile?, List<CqlCompilerExcepti
     override fun doAnnotate(file: PsiFile?): List<CqlCompilerException>? {
         val fileContents = file!!.text
 
-        /*
-            Some libraries use ServiceLoader to detect and load implementations. For this to work in a plugin,
-            the context class loader must be set to the plugin's classloader and restored afterwards with the
-            original one around initialization code:
-         */
-        val currentThread = Thread.currentThread()
-        val originalClassLoader = currentThread.contextClassLoader
-        val pluginClassLoader = this.javaClass.classLoader
-        try {
-            currentThread.contextClassLoader = pluginClassLoader
-            return try {
-                // ModelManager and Library Manager must be created in this contxt to make sure ServiceLoaders are ready
-                if (modelManager == null)
-                    modelManager = ModelManager()
-                if (libraryManager == null)
-                    libraryManager = LibraryManager(modelManager)
+        return ApplicationManager.getApplication().runReadAction<List<CqlCompilerException>?> {
+            /*
+                Some libraries use ServiceLoader to detect and load implementations. For this to work in a plugin,
+                the context class loader must be set to the plugin's classloader and restored afterwards with the
+                original one around initialization code:
+             */
+            val currentThread = Thread.currentThread()
+            val originalClassLoader = currentThread.contextClassLoader
+            val pluginClassLoader = this.javaClass.classLoader
+            try {
+                currentThread.contextClassLoader = pluginClassLoader
+                try {
+                    // ModelManager and Library Manager must be created in this contxt to make sure ServiceLoaders are ready
+                    val modelManager = ModelManager()
+                    val libraryManager = LibraryManager(modelManager).apply {
+                        librarySourceLoader.registerProvider(PsiDirectoryLibrarySourceProvider(file.containingDirectory))
+                    }
 
-                val compiler = CqlCompiler(modelManager, libraryManager)
-                compiler.run(fileContents)
-                compiler.errors + compiler.warnings + compiler.messages
-            } catch (e: IOException) {
-                e.printStackTrace()
-                throw RuntimeException(e)
+                    val compiler = CqlCompiler(modelManager, libraryManager)
+                    compiler.run(fileContents)
+                    compiler.errors + compiler.warnings + compiler.messages
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    throw RuntimeException(e)
+                }
+
+            } finally {
+                currentThread.contextClassLoader = originalClassLoader
             }
-
-        } finally {
-            currentThread.contextClassLoader = originalClassLoader
         }
     }
 
---
+++ src/main/kotlin/org/pathcheck/intellij/cql/PsiDirectoryLibrarySourceProvider.kt
@@ -0,0 +1,55 @@
+package org.pathcheck.intellij.cql
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import org.cqframework.cql.cql2elm.LibrarySourceProvider
+import org.cqframework.cql.cql2elm.model.Version
+import org.hl7.elm.r1.VersionedIdentifier
+import java.io.*
+
+// NOTE: This implementation is naive and assumes library file names will always take the form:
+// <filename>[-<version>].cql
+// And further that <filename> will never contain dashes, and that <version> will always be of the form <major>[.<minor>[.<patch>]]
+// Usage outside these boundaries will result in errors or incorrect behavior.
+class PsiDirectoryLibrarySourceProvider(private val path: PsiDirectory) : LibrarySourceProvider {
+
+    // Returns the version between - and .
+    // <filename>[-<version>].cql
+    private fun parseVersionNumber(fileName: String): Version {
+        return Version(fileName.substring(fileName.indexOf("-")+1, fileName.lastIndexOf(".")))
+    }
+
+    override fun getLibrarySource(libraryIdentifier: VersionedIdentifier): InputStream? {
+        val libraryName = libraryIdentifier.id
+        val libraryVersion = libraryIdentifier.version
+
+        // look for <filename>-<version>.cql
+        val psiFile = path.findFile(listOfNotNull(libraryName, libraryVersion).joinToString("-")+".cql")
+        if (psiFile != null) {
+            return psiFile.text.byteInputStream()
+        }
+
+        // If the file is named correctly, but has no version, consider it the most recent version
+        // look for <filename>.cql
+        val noVersion = path.findFile("$libraryName.cql")
+        if (noVersion != null) {
+            return noVersion.text.byteInputStream()
+        }
+
+        val requestedVersion = if (libraryVersion != null) Version(libraryVersion) else null
+
+        // If the file has a version, make sure it is compatible with the version we are looking for
+        val newestCompatibleVersion = path.files
+            .filter { it.name.startsWith(libraryName) && it.name.endsWith(".cql") }
+            .filter { requestedVersion == null || parseVersionNumber(it.name).compatibleWith(requestedVersion) }
+            .maxByOrNull { parseVersionNumber(it.name) }
+
+
+        if (newestCompatibleVersion != null) {
+            return newestCompatibleVersion.text.byteInputStream()
+        }
+
+        return null
+    }
+}
