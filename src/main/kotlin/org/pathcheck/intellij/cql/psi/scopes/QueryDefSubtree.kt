c752b2a79ab1fb23df5ef36fa1642098054329e1
Vitor Pamplona <vitor@vitorpamplona.com>
2022-10-07 18:52:51 -0400

    Fixing resolve of Query, which should not resolve the querySource identifier from the alias reference (if they are the same)

--- src/main/kotlin/org/pathcheck/intellij/cql/psi/scopes/QueryDefSubtree.kt
+++ src/main/kotlin/org/pathcheck/intellij/cql/psi/scopes/QueryDefSubtree.kt
@@ -4,9 +4,13 @@ import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNamedElement
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.util.elementType
 import org.antlr.intellij.adaptor.SymtabUtils
+import org.antlr.intellij.adaptor.lexer.RuleIElementType
 import org.antlr.intellij.adaptor.psi.IdentifierDefSubtree
 import org.antlr.intellij.adaptor.psi.ScopeNode
+import org.cqframework.cql.gen.cqlLexer
+import org.cqframework.cql.gen.cqlParser
 import org.pathcheck.intellij.cql.CqlLanguage
 
 /** A subtree associated with a query.
@@ -14,12 +18,24 @@ import org.pathcheck.intellij.cql.CqlLanguage
  */
 class QueryDefSubtree(node: ASTNode, idElementType: IElementType) : IdentifierDefSubtree(node, idElementType), ScopeNode {
     override fun resolve(element: PsiNamedElement): PsiElement? {
+        // only resolves if it comes from the alias subtrue
+        var checkElementIsFromAlias: PsiElement = element
+        while (checkElementIsFromAlias.parent !is QueryDefSubtree) {
+            if (checkElementIsFromAlias.node.elementType is RuleIElementType
+                && (checkElementIsFromAlias.node.elementType as RuleIElementType).ruleIndex == cqlParser.RULE_querySource) {
+                // passes it forward.
+                return (parent.context as? ScopeNode)?.resolve(element)
+            }
+
+            checkElementIsFromAlias = checkElementIsFromAlias.parent
+        }
+        
         return listOf(
             "/query/sourceClause/aliasedQuerySource/alias/identifier/IDENTIFIER",
             "/query/sourceClause/aliasedQuerySource/alias/identifier/DELIMITEDIDENTIFIER",
             "/query/sourceClause/aliasedQuerySource/alias/identifier/QUOTEDIDENTIFIER",
         ).firstNotNullOfOrNull {
-            return SymtabUtils.resolve(this, CqlLanguage, element, it)
+            SymtabUtils.resolve(this, CqlLanguage, element, it)
         }
     }
 }
