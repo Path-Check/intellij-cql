be4e8f7a9ac827abab71bd87f8a8e3ba7a81e63e
Vitor Pamplona <vitor@vitorpamplona.com>
2023-04-19 09:10:29 -0400

    Migrating to Java 17 because of the new IntelliJ version

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
 // Set the JVM language level used to compile sources and generate files - Java 11 is required since 2020.3
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
